27ae001d8704a4a3e0f01c2ad2f9744a15ae7146
richard gichuki <chadgichuki@gmail.com>
2022-06-16 13:56:18 +0300

    Palindrome solution 1

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+  if the reversed string is the same as the input
+    return true
+  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  we reverse the string using our reverseString function
+  we test if the reversed word is similr to the initial word.
+  We return true/false accordingly
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +36,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 }
 
 module.exports = isPalindrome;
